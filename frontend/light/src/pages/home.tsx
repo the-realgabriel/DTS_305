8e8283334cfba8ca6467730d6c4003a991f25284
the-realgabriel <theopgabrie@gmail.com>
2025-11-12 19:29:50 +0100

    modified:   frontend/light/src/App.tsx 	new file:   frontend/light/src/pages/home.tsx 	modified:   frontend/light/vite.config.ts

---
+++ frontend/light/src/pages/home.tsx
@@ -0,0 +1,11 @@
+
+
+export default function Home(){
+    return(
+        <>
+            <div className="justify-center">
+                <h1 className="text-center">Welcome</h1>
+            </div>
+        </>
+    )
+}
--- frontend/light/vite.config.ts
+++ frontend/light/vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tailwindcss from 'tailwindcss'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
